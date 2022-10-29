b89161c21f64078f45c5491d6f53b867dec93636
Shankar Rao Pandala <shankar.pandala@gmail.com>
2022-10-29 21:52:14 +0530

    fixed indentation issue

--- test_hello.py
+++ test_hello.py
@@ -2,13 +2,13 @@ from hello import toyou, add, subtract
 
 
 def setup_function(function):
-    print("Running Setup: %s" % function.__name__)
-    function.x = 10
+	print("Running Setup: %s" % function.__name__)
+	function.x = 10
 
 
 def teardown_function(function):
-    print("Running Teardown: %s" % function.__name__)
-    del function.x
+	print("Running Teardown: %s" % function.__name__)
+	del function.x
 
 
 ### Run to see failed test
@@ -16,4 +16,4 @@ def teardown_function(function):
 #    assert add(test_hello_add.x) == 12
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+	assert subtract(test_hello_subtract.x) == 9
